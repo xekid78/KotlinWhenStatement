c469026acd3cf6b7931b16741e430ed0bd320b36
Makoto Tanabe <xekid78@outlook.com>
2018-04-10 16:55:21 +0900

    2nd commit
    when文の記述を一部変更

--- when.kt
+++ when.kt
@@ -3,14 +3,13 @@ import java.util.Random
 fun main(args: Array<String>) {
     val r = Random()
     val num = r.nextInt(12)
-    val omikuji =
-        when (num) {
-            0 -> "大吉"
-            1, 2 -> "中吉"
-            in 3..6 -> "小吉"
-            in 7..9 -> "吉"
-            10, 11 -> "凶"
-            else -> "大凶"
-        }
-        println(omikuji)
+    val omikuji = when (num) {
+        0 -> "大吉"
+        1, 2 -> "中吉"
+        in 3..6 -> "小吉"
+        in 7..9 -> "吉"
+        10, 11 -> "凶"
+        else -> "大凶"
+    }
+    println(omikuji)
 }
